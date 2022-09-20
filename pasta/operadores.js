2cc906ca281cef1ba49d5b5985a3a2ef1de22b84
Alunos <aluno.etec@etecEmbu.com.br>
2022-09-19 22:31:08 -0300

    estilização site

---
+++ pasta/operadores.js
@@ -0,0 +1,98 @@
+// console.log ("ola mundo")
+// let n1 =20;
+// let n2 =10;
+// let soma = n1+n2;
+// console.log(soma);
+
+// let n3 = 5;
+// let n4 = 8;
+// let subtraçao = n3 - n4
+// console.log(subtraçao)
+
+// let n5 = 5;
+// let n6 = 8;
+// let divisao = n3 / n4
+// console.log(divisao)
+
+
+// let n13 = 10;
+// console.log(++n13)
+// console.log(--n13)
+
+
+//exemplo operadores de comparação
+//maior >
+
+let n1 = 20; 
+let n2 =14;
+console.log(n1 > n2);
+
+//maior ou igual(fira-code)
+let n3 = 20;
+let n4 = 21;
+console.log(n3 >= n4);
+
+//< menor 
+let n5 = 20
+let n6 = 21
+console.log(n5 < n6);
+
+//menor igual <=
+let n7 = 20
+let n8 = 21
+console.log(n7 <= n8);
+
+// = =
+let n9 = 21
+let n10 = 15
+console.log(n9 == n10);
+
+// !=
+let n11 = 15
+let n12 = 16
+console.log(n11 != n12);
+
+// === 3 iguais 
+let n13 = 18
+let n14 = 15
+console.log(n13 === n14);
+
+// !==
+//let n15 = 16
+//let n16 = 20
+//console.log(n15 !== n16);
+
+//const idade = 16;
+//if (idade >= 18) {
+  //alert('A pessoa é maior de idade');
+//} //else {
+  //alert('A pessoa não é maior de idade'); 
+//}
+ 
+//const anolancamento = 1980;
+
+//if(anolancamento < 2000);{
+ // alert('este carro é antigo');
+//} else if(anolancamento > 2015){
+  //alert('este carro é novo');
+//} else{
+  //alert('este carro é intermediario');
+//}
+
+// const pesodapessoa = 50
+
+// if(pesodapessoa <= 30 );{
+//   alert('a pessoa é leve');
+// }else if(peso >=30 && peso <= 70){
+//   alert('a pessoa esta no peso normal');
+// }else if( peso >= 71 && peso <= 100){
+//   alert('a pessoa ta com sobre peso')
+// }else{alert('a pessoa ta com sobre peso')};
+
+// const nome = 'JEAN';
+// alert('Bem vindo ao sistema' + nome); 
+  
+
+
+
+
